b03eb5a8619eec4f35777adf7691fc81b1490acc
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-04 14:09:31 -0300

    feat(fetch-api): fetching trailers

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react"
+
+interface Trailer {
+    id: number,
+    name: string,
+    description: string,
+    youtubeId: string,
+    categories: string[]
+}
+
+interface ApiResponse {
+    trailers: Trailer[];
+}
+
 export default function Home() {
+  const [trailers, setTrailers] = useState<Trailer[] | null>(null);
+
+  useEffect(() => {
+    const fetchTrailers = async () => {
+        const res = await fetch('https://api-trailerflix.vercel.app/trailers')
+        const data: ApiResponse = await res.json()
+        setTrailers(data.trailers)
+    }
+    
+    fetchTrailers();
+  }, []);
+
   return (
-    <div>Home</div>
+    <div>
+        {trailers ? (
+          trailers.map((trailer) => (
+            <p key={trailer.id}>{trailer.name}</p>
+        ))
+        ) : (
+          <p>Carregando...</p>
+        )}
+    </div>
   )
 }
