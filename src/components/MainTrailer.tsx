af5fd884dda8c48abbecd1307d69711847d0919e
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-06 17:07:33 -0300

    refactor(main trailer): changing classNames

--- src/components/MainTrailer.tsx
+++ src/components/MainTrailer.tsx
@@ -12,12 +12,14 @@ export default function MainTrailer({id, name, youtubeId, description, categorie
   return (
     <section className="main_trailer" key={id}>
         <iframe width="560" height="315" src={`https://www.youtube.com/embed/${youtubeId}?si=l66iGm3XYwGtjHNK`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-        <h1 className="trailer_title">{name}</h1>
-        <p className="trailer_description">{description}</p>
-        <div className="trailer_categories">
-            {categories.map((category) => (
-            <p>{category}</p>
-            ))}
+        <div className="trailer_details">
+          <h1 className="title">{name}</h1>
+          <p className="description">{description}</p>
+          <div className="categories">
+              {categories.map((category) => (
+              <p>{category}</p>
+              ))}
+          </div>
         </div>
     </section>
   )
