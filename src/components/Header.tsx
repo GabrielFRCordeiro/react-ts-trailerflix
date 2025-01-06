1f219d3d24fb1adfebf482d249ae87e98793c5c8
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-06 15:12:21 -0300

    feat(logo): redirecting to home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import '../styles/components/header.sass'
 import logo from '../assets/img_logo.svg'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <section className="header">
-        <img src={logo} alt="TrailerFlix logo" className='img_logo' />
+        <Link to='/'>
+          <img src={logo} alt="TrailerFlix logo" className='img_logo' />
+        </Link>
         <label htmlFor="t_field_search">
           <input type="text" name="t_field_search" id="t_field_search" placeholder='Search trailer...' />
           <i className="bi bi-search"></i>
