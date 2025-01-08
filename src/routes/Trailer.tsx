acc751b4c528b3adfaaad18fc3c57b0378069bcc
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-08 11:36:08 -0300

    fix(Trailer): fixing useEffect rendering

--- src/routes/Trailer.tsx
+++ src/routes/Trailer.tsx
@@ -37,7 +37,7 @@ export default function Trailer() {
     }
     
     fetchTrailers();
-  }, [featuredTrailerId, trailerId]);
+  }, [trailerId]);
 
   const handleCardClick = (id: number) => {
     setFeaturedTrailerId(id);
